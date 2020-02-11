bd86648a8136eaaddf3e21c863c4190531c62510
unknown <ahmedosamabedawy@gmail.com>
2020-02-11 05:30:46 +0200

    Updates

---
+++ precache-manifest.7ae892a294cd86665eefdcd201fa74f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fc7f20d777238740a603c1fb3f7666",
+    "url": "/AIESECTask/index.html"
+  },
+  {
+    "revision": "e0d015e2216af286b2a3",
+    "url": "/AIESECTask/static/css/main.4dceae31.chunk.css"
+  },
+  {
+    "revision": "2f2600be2d218dbee6af",
+    "url": "/AIESECTask/static/js/2.a21de790.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/AIESECTask/static/js/2.a21de790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d015e2216af286b2a3",
+    "url": "/AIESECTask/static/js/main.7bebed0d.chunk.js"
+  },
+  {
+    "revision": "42c0e4d244a375cf13bf",
+    "url": "/AIESECTask/static/js/runtime-main.eeb08d0d.js"
+  }
+]);
